a03e643bea6cd228d302a94550146716520228e9
satv2025 <traplegionrecords2020@gmail.com>
2025-11-09 00:56:13 -0300

    act

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -5,4 +5,5 @@ const SUPABASE_URL = "https://dpmqzuvyygwreqpffpca.supabase.co";
 const SUPABASE_ANON_KEY =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwbXF6dXZ5eWd3cmVxcGZmcGNhIiwicm9zZSI6ImFub24iLCJpYXQiOjE3NjI1NDk4MjcsImV4cCI6MjA3ODEyNTgyN30.BxgH_mcXgjwuiRz8yhwpxnF-UDkLyFpl16Yo0sz-0Qk";
 
-export const supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Crear cliente global (disponible en window)
+window.supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
